8ef15e2dc0b28ebc408887f30695c141db9feeba
Brad Hudson <bradley.hudson@careerbuilder.com>
2015-06-03 14:19:11 -0400

    ajax works for get

--- popup.js
+++ popup.js
@@ -2,5 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
    var voteButton = document.getElementById('vote');
   voteButton.addEventListener('click', function() {
     document.getElementById("test").innerHTML = "New text!";
+    $.get("http://localhost:4567/vote", function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
+    });
   })
 });

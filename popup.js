fe1aab5fecb45875bbd4313aeda350099b767df8
Brad Hudson <bradley.hudson@careerbuilder.com>
2015-06-03 14:34:45 -0400

    works locally and posts choice

--- popup.js
+++ popup.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
    var voteButton = document.getElementById('vote');
   voteButton.addEventListener('click', function() {
-    document.getElementById("test").innerHTML = "New text!";
-    $.get("http://localhost:4567/vote", function(data, status){
+    $.get("http://localhost:4567/vote", { choice: document.getElementById("entry").value } ,function(data, status){
         alert("Data: " + data + "\nStatus: " + status);
     });
   })

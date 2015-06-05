6207878766dac819660b9fe683a2de3138525c1c
Brad Hudson <bradley.hudson@careerbuilder.com>
2015-06-05 07:53:33 -0400

    adjusting frontend/changed name/deployed to heroku

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
    var voteButton = document.getElementById('vote');
   voteButton.addEventListener('click', function() {
-    $.get("http://localhost:4567/vote", { choice: document.getElementById("entry").value } ,function(data, status){
+    $.get("https://glacial-hollows-4569.herokuapp.com/vote", { choice: document.getElementById("entry").value } ,function(data, status){
         alert("Data: " + data + "\nStatus: " + status);
     });
   })

4e6e5173bcae9b85dc439c48b53316f2e2d6fb73
Brad Hudson <bradley.hudson@careerbuilder.com>
2015-06-06 10:04:22 -0400

    adding yelp

--- popup.js
+++ popup.js
@@ -1,4 +1,12 @@
+function getYelpSuggestion(){
+	 $.get("https://glacial-hollows-4569.herokuapp.com/",function(data){
+        alert("Data: " + data);
+        // document.getElementById("yelpsuggestion").value = data;
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
+	getYelpSuggestion()
    var voteButton = document.getElementById('vote');
   voteButton.addEventListener('click', function() {
     $.get("https://glacial-hollows-4569.herokuapp.com/vote", { choice: document.getElementById("entry").value } ,function(data, status){

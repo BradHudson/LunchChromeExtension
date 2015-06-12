2705b28f90a4fa8f40bfbc05c11623d38681584e
Brad Hudson <bradley.hudson@careerbuilder.com>
2015-06-12 10:08:13 -0400

    added loading icon

--- popup.js
+++ popup.js
@@ -9,6 +9,9 @@ document.addEventListener('DOMContentLoaded', function() {
 	// getYelpSuggestion()
    var voteButton = document.getElementById('vote');
   voteButton.addEventListener('click', function() {
+    document.getElementById("main").style.display= 'none';
+    document.getElementById("loading").style.visibility = 'visible';
+    document.getElementById("loading").style.display = 'block';
     $.get("https://glacial-hollows-4569.herokuapp.com/vote", { choice: document.getElementById("entry").value } ,function(data, status){
         alert("Data: " + data + "\nStatus: " + status);
     });
